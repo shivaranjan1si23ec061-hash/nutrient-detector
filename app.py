976d6db1e74ae5f36b9aeed95f318de509ef2d02
shivaranjan1si23ec061-hash <shivaranjan.1si23ec061@gmail.com>
2025-11-15 06:44:27 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time
 import random
 import plotly.express as px
 import plotly.graph_objects as go
-
 st.set_page_config(page_title="AI Vitamin Deficiency Detector",
                    layout="wide",
                    page_icon="🧬")
